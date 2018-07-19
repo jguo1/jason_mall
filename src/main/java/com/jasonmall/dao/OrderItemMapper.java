df8d0fea49e54c97977be5de30ea3a297f4e4679
Jason Guo <271726514@qq.com>
2018-07-20 01:52:37 +0800

    dao mapper pojo add

---
+++ src/main/java/com/jasonmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.jasonmall.dao;
+
+import com.jasonmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
